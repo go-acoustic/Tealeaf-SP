2acc8e7b7f1788558c60b36c6b1c16e055c4e781
Omar Hernandez <omar.hernandez@acoustic.co>
2021-05-06 15:09:06 -0700

    EAS-289: Macro not working, need to create a separate swift package for debug vs release.
    Release - https://github.com/acoustic-analytics/Tealeaf-SP
    Debug - https://github.com/acoustic-analytics/TealeafDebug-SP

--- Package.swift
+++ Package.swift
@@ -10,37 +10,9 @@
 // prohibited.
 
 import PackageDescription
-#if DEBUG
-    print("Using Tealeaf Debug version")
-    let package = Package(
-        name: "Tealeaf",
-        products: [
-            // Products define the executables and libraries a package produces, and make them visible to other packages.
-            .library(
-                name: "Tealeaf",
-                targets: ["Tealeaf", "EOCore"]),
-        ],
-        dependencies: [
-            // Dependencies declare other packages that this package depends on.
-            // .package(url: /* package url */, from: "1.0.0"),
-            //
-        ],
-        targets: [
-            // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-            // Targets can depend on other targets in this package, and on products in packages this package depends on.
-            .binaryTarget(
-                name: "Tealeaf",
-                url: "https://github.com/acoustic-analytics/IBMTealeaf/releases/download/10.6.73/Tealeaf_XCFrameworks_Debug.zip",
-                checksum: "34a1b645136ace463f0dc4a4293477187dc8511e3b309a0fd6fdd8121575981b"),
-            .binaryTarget(
-                name: "EOCore",
-                url: "https://github.com/acoustic-analytics/EOCore/releases/download/2.3.33/EOCore_XCFrameworks_Debug.zip",
-                checksum: "4b0a2a39c73cd8512b3e6e876609f9fb50e1a718c9f9e42503d2713e81a794b8"),
-        ]
-    )
-#else
-    print("Using Tealeaf NON-Debug version")
-    let package = Package(
+
+print("Using Tealeaf NON-Debug version, if you need debug version use https://github.com/acoustic-analytics/TealeafDebug-SP")
+let package = Package(
     name: "Tealeaf",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -66,4 +38,3 @@ import PackageDescription
             checksum: "4afc44c861331b3aafc9b5f22f90210528190fd85e7f3403962583e72e26ee13"),
     ]
 )
-#endif
